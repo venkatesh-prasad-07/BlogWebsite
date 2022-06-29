34b1d71c06a1ee96e7fdc9754632cbe4a0a62bc9
Venkatesh Prasad <76904939+venkatesh-prasad-07@users.noreply.github.com>
2022-06-29 18:05:26 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z-=r@v9g68e_c2q=x9u%ce&^tkh+ysd&-ug5j33ofm)31bc%x2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','the-blog-space.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
